d024e6f0357b14814c0326eaa1c4a7821cc1532e
dstiller-im-gm <107100347+dstiller-im-gm@users.noreply.github.com>
2022-06-08 09:27:01 +0200

    Update streamlit_app.py
    pre-populate the list of multi-select to set an example for the customer

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #User Interaction
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado','Strawberries'])
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
